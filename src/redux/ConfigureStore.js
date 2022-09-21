b88e6e87ff5fd11f9363903feebebe35de8981b8
clintonjosephs <clintonjosephs26@gmail.com>
2022-09-21 08:58:21 +0000

    Setup up redux store and combined reducer

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,2 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import {combineReducers, legacy_createStore as createStore } from "redux";
 import logger from 'redux-logger';
+import messageReducer from "./chat/chat";
+import usersReducer from "./user/user";
+
+const reducer = combineReducers({
+    usersReducer,
+    messageReducer,
+});
+
+const store = createStore(reducer, logger);
+
+export default store;
