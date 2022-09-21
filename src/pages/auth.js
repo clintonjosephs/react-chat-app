cb880ebd9b2463b319baf00acb03bdd2bf8ebefe
clintonjosephs <clintonjosephs26@gmail.com>
2022-09-21 11:04:04 +0000

    Add current user and logged in tracker to the user state object

---
+++ src/pages/auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Auth = () => (
+  <>
+    <div>This is the authentication page</div>
+    <div>
+      <h5>This is the authentication button</h5>
+    </div>
+  </>
+);
+
+export default Auth;
--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -1,10 +1,15 @@
 // transaction addresses
 const CREATE_USER = 'CREATE_USER';
 const FETCH_USER = 'FETCH_USER';
+const SET_CURRENT_USER = 'SET_CURRENT_USER';
+const FETCH_CURRENT_USER = 'FETCH_CURRENT_USER';
+const LOGGED_IN = 'LOGGED_IN';
 
 // initial state
 const initialState = {
   users: [],
+  currentUser: null,
+  isLoggedIn: false,
 };
 
 // syncronous actions
@@ -16,6 +21,19 @@ export const createUser = (userDetails) => ({
 export const fetchUser = (userId) => ({
   type: 'FETCH_USER',
   payload: userId,
+});
+
+export const setCurrentUser = (userData) => ({
+  type: 'SET_CURRENT_USER',
+  payload: userData,
+});
+
+export const fetchCurrentUser = () => ({
+  type: 'FETCH_CURRENT_USER',
+});
+
+export const loginStatus = () => ({
+  type: 'LOGGED_IN',
 });
 
 const usersReducer = (state = initialState, action) => {
@@ -30,6 +48,12 @@ const usersReducer = (state = initialState, action) => {
         }
         return null;
       });
+    case SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    case FETCH_CURRENT_USER:
+      return state.currentUser;
+    case LOGGED_IN:
+      return { ...state, isLoggedIn: !state.isLoggedIn };
     default:
       return state;
   }
